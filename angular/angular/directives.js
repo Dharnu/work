4e5ca7359b54e35b4499593d2f705ece8828ccdb
Hemant <HemantP@muralie.e2info.com>
2016-03-11 20:25:55 +0530

    non functional angular code

--- angular/angular/directives.js
+++ angular/angular/directives.js
@@ -12,5 +12,29 @@ return{
 	templateUrl:"./templates/hotel-description.html",
 };
 });
+app.directive('hotelImages',function(){
+return{
+	restrict:'E',
+	templateUrl:"./templates/hotel-images.html",
+	contoller:function($scope){
+			this.facilities ="GYM";
+			this.setFacilities=function($facilities){
+				console.log($scope);
+				this.facilities=facilities;
+				console.log("yo");
+				};
+			this.doesContain=function($facility){
+				console.log(this.facilities);
+				console.log("hi");
+				if(this.facilities.indexOf(facility)!==-1){
+					return 0;	
+					}
+				else return 1;
+				}
+			},
+	controllerAs:"hImages"
+};
+});
+
 }
 )();
